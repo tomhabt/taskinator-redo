9dd0062c0de923b2290876a54674064c4f36439e
Thomas Habtemariam <84083304+tomhabt@users.noreply.github.com>
2021-12-30 17:10:43 -0500

    add ability to update and delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 var pageContentEl = document.querySelector("#page-content");
 var taskIdCounter = 0;
 
@@ -185,8 +187,34 @@ var deleteTask = function(taskId) {
     taskSelected.remove();
 };
 
+
+// 7. TASK STATUS CHANGER  FUNCTION
+var taskStatusChangeHandler = function(event) {
+
+    // get the task item's id
+    var taskId = event.target.getAttribute("data-task-id");
+  
+    // get the currently selected option's value and convert to lowercase
+    var statusValue = event.target.value.toLowerCase();
+  
+    // find the parent task item element based on the id
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    if (statusValue === "to do") {
+        tasksToDoEl.appendChild(taskSelected);
+      } 
+      else if (statusValue === "in progress") {
+        tasksInProgressEl.appendChild(taskSelected);
+      } 
+      else if (statusValue === "completed") {
+        tasksCompletedEl.appendChild(taskSelected);
+      }
+};
+  
+
 // event listeners
 
     pageContentEl.addEventListener("click", taskButtonHandler);
 
     formEl.addEventListener("submit", taskFormHandler);
+
+    pageContentEl.addEventListener("change", taskStatusChangeHandler);
